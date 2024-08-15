0d3db4c3b35e7fbe80514250acdb7f260c93d457
noorulainnoorulain229 <noorulainnoorulain229@gmail.com>
2024-08-15 10:04:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+    //  question no 01 
+
+
+    // FIND THE AREA  OF RECTANGLE WHERE THE LENGTH IS 5 AND THE WIDTH IS 8 . 
+    let div = document.getElementById("abc");
+      let length = 5;
+      let width = 8;
+      let area = length*width;
+      document.write("area of rectangle is " ,area);
+   
+
+
+
+    //   question no 02  
+    // FIND THE AREA  OF TRAINGLE WHERE THE BASE IS 4  AND HIEGHT  IS 3.
+
+    let  base = 4;
+    let height = 3;
+    let areaoftraingle = (base * height) / 2
+   document.write("areaoftraingle  is",areaoftraingle);
+
+
+
+   // question number 03 
+  //  FIND THE AREA OF CIRCLE WHERE THE RADIUS IS 3.
+  let radius =  3;
+  const PI =3.14;
+  let areaOfcircle = PI  * radius**2;
+  document.write("areaOfcircle is ",areaOfcircle);
+
+
+  // question no 4 
+  //  CONVERT TEMPERATURES FROM CELSIUS TO  FEHERNHEIT AND FEHERNHEIT TO CELSIUS.
+
+  let celsius = 37;
+  let fehernheit = (9/5 * celsius) +32;
+  document.write("fehernheit", fehernheit);
+
+
+
+  let fehernhei =  100;
+  let celsiu =  (F - 32 ) * 5/9;
+  document.write("celsiu", celsiu);
